eee90e562e154c17dfe09bf7de574d2ea9ba7b9e
WanNingxi <wan_ningxi@163.com>
2016-01-28 12:05:58 -0500

    did not resolve the read the ip_address and write in a file from mysql

--- connect_database.py
+++ connect_database.py
@@ -13,9 +13,16 @@ conn= MySQLdb.connect(
 cursor = conn.cursor()
 
 #获得表中有多少条数据
-cursor.execute("select * from polls_status")
+cursor.execute("select * from polls_ip_address")
 row = cursor.fetchall()
-print row
+
+a= dict(row)
+print a
+
+for x in a.values():
+	filename = open('ip.txt','a+')
+	filename.write(x)
+	filename.close()
 
 cursor.close()
 conn.close()
